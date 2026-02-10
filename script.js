2f1d92249432bd709ed01bd1e1c4e3143f38dd13
SadiqurApon <sadiqurrahmanapon34@gmail.com>
2026-02-10 18:58:14 +0600

    file added

---
+++ script.js
@@ -0,0 +1,9 @@
+const person = {
+    name: "John", 
+    fruit : "Apple",
+    dish : "Pizza",
+    friends : ["Yafi", "Sabbir", "Munna"],
+    isRich : false,
+    money : 1000,
+};
+console.log(person);
